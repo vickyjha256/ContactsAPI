ee9dcb01ff04436625c51cf51bb9e9adf83251a7
Vicky Jha <vickyjha256@gmail.com>
2024-08-10 21:12:39 +0530

    Contacts API Code uploaded

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,97 @@
+﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController] // Here, we use this annotation to tell controller that it's an API controller not a MVC controller.
+    //[Route("api/contacts")] // Here we gave the route name. Below is the another way of giving route name.
+    [Route("api/[controller]")] // It will act same as above because it takes the controller name. Since controller name is same as above.
+
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactsController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet] // Since we are using async await so we have to wrap "IActionResult" inside Task<>
+        public async Task<IActionResult> GetAllContacts()
+        {
+            return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpGet("GetContact")]
+        //[Route("{id:guid}")]
+        public async Task<IActionResult> GetContact( Guid id) 
+        //public async Task<IActionResult> GetContact([FromRoute] Guid id) 
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+
+            if (contact != null)
+            {
+                return Ok(contact);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost] // Since we are using async await so we have to wrap "IActionResult" inside Task<>
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                FullName = addContactRequest.FullName,
+                Email = addContactRequest.Email,
+                Phone = addContactRequest.Phone,
+                Address = addContactRequest.Address,
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")] // Here, we add the id in the url parameter. and this 'id' should be matched with below type of Guid 'id'.
+        public async Task<IActionResult> UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+            if (contact != null)
+            {
+                contact.FullName = updateContactRequest.FullName;
+                contact.Email = updateContactRequest.Email;
+                contact.Phone = updateContactRequest.Phone;
+                contact.Address = updateContactRequest.Address;
+
+                await dbContext.SaveChangesAsync();
+                return Ok(contact);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] Guid id)
+        {
+            var contact = await dbContext.Contacts.FindAsync(id);
+
+            if (contact != null)
+            {
+                dbContext.Contacts.Remove(contact);
+                await dbContext.SaveChangesAsync();
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+}
